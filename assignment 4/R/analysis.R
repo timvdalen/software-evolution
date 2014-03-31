daec80d66fabed71fb8eee03346d063c95ec7e4f
Stef van Schuylenburg <s.b.v.schuylenburg@student.tue.nl>
2014-03-31 14:27:49 +0200

    Fix of R script
    The analysis script does now work correctly

--- assignment 4/R/analysis.R
+++ assignment 4/R/analysis.R
@@ -2,32 +2,41 @@
 library("epitools")
 
 # reading input file (expecting the file on buildinfo.csv)
-buildinfo = read.csv("buildinfo.csv")[,-1][,-1]
-X <- as.matrix(buildinfo)
-
-# doing the statistical computations
-ct <- chisq.test(X)
-or <- oddsratio(X)
+buildinfo = read.csv("buildinfo.csv")
 
 # creating matrix OUT which will be saved to a file
-OUT <- matrix(numeric(0), 0, 7) 
+OUT <- matrix(numeric(0), 0, 8) 
 colnames(OUT) = 
-  c("p of ct", "residual 1", "residual 2", "residual 3", "residual 4",
+  c("slug", "p of ct", "residual 1", "residual 2", "residual 3", "residual 4",
     "measure of or", "p of or")
 
-for (i in 1:nrow(X)) {
-  if (X[i,1] < 5 || X[i,2] < 5 || X[i,3] < 5 || X[i,4] < 5) {
+for (i in 1:nrow(buildinfo)) {
+
+  # reading the buildinfo for row i
+  X <- matrix(c(buildinfo[i, 'commits.passed'], buildinfo[i, 'commits.failed'],
+    buildinfo[i, 'pr.passed'], buildinfo[i, 'pr.failed']), 2, 2)
+
+  if (X[1, 1] < 5 || X[1, 2] < 5 || X[2, 1] < 5 || X[2, 2] < 5) {
     # too few builds
-    row <- c(NA, NA, NA, NA, NA)
+    row <- c(NA, NA, NA, NA, NA, NA, NA)
   } else { # there are enough builds
-    row <- c(ct$p.value, ct$residual[i, 1], ct$residual[i, 2],
-      ct$residual[i, 3], ct$residual[i, 4],
-      or$measure[i, 1], or$p.value[i, 1])
+    # calculating values
+    ct <- chisq.test(X)
+    or <- oddsratio(X)
+
+    # creating output data
+    row <- c(ct$p.value, ct$residual[1, 1], ct$residual[1, 2],
+      ct$residual[2, 1], ct$residual[2, 2], or$measure[2, 1], or$p.value[2, 1])
   }
 
-  ADD <- matrix(row, ncol = 7)
+  # creating a row for the output data
+  ADD <- matrix(ROW, ncol = ncol(OUT), nrow = 1)
+  ADD[1, 1] <- buildinfo[i, 'slug']
+  for (j in 1:length(row)) {
+    ADD[1, 1 + j] = row[j]
+  }
   OUT <- rbind(OUT, ADD) 
 }
 
 rdata <- as.table(OUT)
-write.csv(rdata, "rdata.csv")
+write.csv(rdata, "rdata.csv", quote = FALSE, row.names = FALSE)
